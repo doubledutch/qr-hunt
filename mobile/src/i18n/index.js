020d31e19201b10d3842e663e47c9a101d3062c9
Adam Liechty <adam@doubledutch.me>
2019-03-01 16:54:32 -0800

    fix i18n paths

--- mobile/src/i18n/index.js
+++ mobile/src/i18n/index.js
@@ -1,12 +1,12 @@
-import enUS from './en-us/qr-hunt.admin.json'
+import enUS from './en-us/qr-hunt.mobile.json'
 
-import arINT from './ar-int/qr-hunt.admin.json'
-import deDE from './de-de/qr-hunt.admin.json'
-import esES from './es-es/qr-hunt.admin.json'
-import frFR from './fr-fr/qr-hunt.admin.json'
-import itIT from './it-it/qr-hunt.admin.json'
-import jaJP from './ja-jp/qr-hunt.admin.json'
-import nlNL from './nl-nl/qr-hunt.admin.json'
+import arINT from './ar-int/qr-hunt.mobile.json'
+import deDE from './de-de/qr-hunt.mobile.json'
+import esES from './es-es/qr-hunt.mobile.json'
+import frFR from './fr-fr/qr-hunt.mobile.json'
+import itIT from './it-it/qr-hunt.mobile.json'
+import jaJP from './ja-jp/qr-hunt.mobile.json'
+import nlNL from './nl-nl/qr-hunt.mobile.json'
 
 export default {
   '': enUS,
